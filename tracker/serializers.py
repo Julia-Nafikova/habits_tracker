84b5940a1596fab1078726a263c84b720d06f470
Nafikova_Julia <nafikova91@yandex.ru>
2025-04-11 14:28:27 +0300

    add serializers

---
+++ tracker/serializers.py
@@ -0,0 +1,19 @@
+from django.core.validators import MaxValueValidator
+from rest_framework import serializers
+
+from tracker.models import UsefulHabit, PleasantHabit
+
+
+class UsefulHabitSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = UsefulHabit
+        fields = "__all__"
+
+
+class PleasantHabitSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = PleasantHabit
+        fields = "__all__"
+        
