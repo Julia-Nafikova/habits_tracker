96988ec0c76fd228670c6f0e1e3f765e64a8b92e
Nafikova_Julia <nafikova91@yandex.ru>
2025-04-15 11:06:09 +0300

    add update UsefulHabit

--- tracker/views.py
+++ tracker/views.py
@@ -32,10 +32,11 @@ class UsefulHabitDetailView(generics.RetrieveAPIView):
     serializer_class = UsefulHabitSerializer
 
 
-class UsefulHabitDeleteView(generics.DestroyAPIView):
+class UsefulHabitUpdateView(generics.UpdateAPIView):
     queryset = UsefulHabit.objects.all()
     serializer_class = UsefulHabitSerializer
 
-    # class UsefulHabitUpdateView(generics.UpdateAPIView):
-    #     queryset = UsefulHabit.objects.all()
-    #     serializer_class = UsefulHabitSerializer
+
+class UsefulHabitDeleteView(generics.DestroyAPIView):
+    queryset = UsefulHabit.objects.all()
+    serializer_class = UsefulHabitSerializer
