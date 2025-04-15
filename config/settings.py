7e7b6c86bd1a180b7747673d18f50946f5dff43d
Nafikova_Julia <nafikova91@yandex.ru>
2025-04-15 12:57:39 +0300

    set documentation and cors

--- config/settings.py
+++ config/settings.py
@@ -27,11 +27,14 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'users',
     'tracker',
+    'corsheaders',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -115,3 +118,14 @@ SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
 }
+
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:8000',
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://localhost:8000",
+]
+
+CORS_ALLOW_ALL_ORIGINS = False
